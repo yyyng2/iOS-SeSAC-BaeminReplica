269ea752250749a91a5a9899d6f9c49615809d10
yyyng2 <yyyng2@gmail.com>
2022-07-08 18:40:44 +0900

    fix layout

--- BaeminReplica/ViewController.swift
+++ BaeminReplica/ViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topViewBackground: UIView!
     @IBOutlet weak var menuButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        topViewBackground.layer.cornerRadius = 10
     }
+    
+    
+    
 
-
+    
 }
 
