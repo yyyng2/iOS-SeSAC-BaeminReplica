f694dd3f2bc22419ae6ffd1d80bd40ec796579c1
yyyng2 <yyyng2@gmail.com>
2022-07-11 14:33:34 +0900

    add NavigationController

--- BaeminReplica/ViewController.swift
+++ BaeminReplica/ViewController.swift
@@ -10,19 +10,16 @@ import UIKit
 class ViewController: UIViewController {
     
 
-    @IBOutlet weak var addressButton: UIButton!
+
     @IBOutlet weak var topViewBackground: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        topViewBackground.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
         topViewBackground.layer.cornerRadius = 10
 
     }
         var addressArray: [String] = ["대구", "서울", "부산", "광주", "경기도", "대전"]
 
-    
-    @IBAction func addressChangeButtonTapped(_ sender: UIButton) {
-        addressButton.setTitle(addressArray[Int.random(in: 0...5)], for: .normal)
-    }
     
     
 }
